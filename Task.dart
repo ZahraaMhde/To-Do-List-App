d5d31018d73037c784ca8f5a737ea51f67e223be
ZahraaMhde <149297538+ZahraaMhde@users.noreply.github.com>
2023-11-26 15:27:40 +0200

    Task.dart

---
+++ Task.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Task {
+  String name;
+  bool isCompleted;
+  DateTime dateTime;
+
+  Task({required this.name, required this.isCompleted, required this.dateTime});
+}
